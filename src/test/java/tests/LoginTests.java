3a2d978a48850ef9ceaae5fc00c0d9102132462a
Vlad <vladmonqa35@gmail.com>
2020-02-02 15:06:29 -0600

    add test

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -22,16 +23,14 @@ public void setUp(){
 }
 
 @Test
-    public void loginTest1(){
+    public void loginTest1() {
     driver.get("http://secure.smartbearsoftware.com/samples/testcomplete12/WebOrders/ login.aspx");
     driver.findElement(By.id("ctl00_MainContent_username")).sendKeys("Tester");
     driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
 
     Assert.assertEquals(driver.getTitle(), "Web Orders");
-    driver.close();
-    driver.quit();
-
-
 }
 
 }
+
+
