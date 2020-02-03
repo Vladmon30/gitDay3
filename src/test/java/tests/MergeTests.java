05bc4e68e7335b3763fb27b00b30d641a3199450
Vlad <vladmonqa35@gmail.com>
2020-02-03 14:52:47 -0600

    resolve stash conflict

--- src/test/java/tests/MergeTests.java
+++ src/test/java/tests/MergeTests.java
@@ -4,8 +4,13 @@ public class MergeTests {
     public void mergeTest1(){
         System.out.println("Testing the merge");
     }
+
     public void stashTest3()
     {
         System.out.println("Testing the stash 3");
+    }
+    public void stashTest4()
+    {
+        System.out.println("stash 4");
     }
 }
