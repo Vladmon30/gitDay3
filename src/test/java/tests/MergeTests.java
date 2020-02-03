6fd688b3a371b52367edb9c37ca31506a7869bfc
Vlad <vladmonqa35@gmail.com>
2020-02-03 14:50:28 -0600

    add stash -mstash@{0}

--- src/test/java/tests/MergeTests.java
+++ src/test/java/tests/MergeTests.java
@@ -4,5 +4,8 @@ public class MergeTests {
     public void mergeTest1(){
         System.out.println("Testing the merge");
     }
-
+    public void stashTest3()
+    {
+        System.out.println("Testing the stash 3");
+    }
 }
