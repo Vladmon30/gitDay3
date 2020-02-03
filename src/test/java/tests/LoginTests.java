d00b2ed77d5c0c88c203a098f59d4692231f4502
Vlad <vladmonqa35@gmail.com>
2020-02-02 22:40:58 -0600

    mistake

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -44,7 +44,7 @@ public void setUp(){
     driver.findElement(By.id("ctl00_MainContent_password")).sendKeys("test" + Keys.ENTER);
         
     String title = driver.getTitle();   
-    Assert.assertEquals(title, "Web Orders Login")
+    Assert.assertEquals(title, "Web Orders Login");
         
 }
 // After method for closing browser
